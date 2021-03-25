b00663ef9993d70dadbe93f65534f9abff1a2b85
Glitch (helpful-honeysuckle-kileskus) <none>
2021-03-25 08:28:34 +0000

    🎱🚃 Updated with Glitch

---
+++ SCREAM.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const { Client, RichEmbed } = require('discord.js');
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const data = new Map();
+
+const scream = new Discord.Client();
+const scream1 = new Discord.Client(); 
+const scream2 = new Discord.Client();
+const scream3 = new Discord.Client();
+const scream4 = new Discord.Client();
+const scream5 = new Discord.Client();
+const scream6 = new Discord.Client();
+const scream7 = new Discord.Client();
+const scream8 = new Discord.Client();
+const scream9 = new Discord.Client();
+const scream0 = new Discord.Client();
+const scream10 = new Discord.Client();
+const scream11 = new Discord.Client();
+const scream12 = new Discord.Client();
+const scream13 = new Discord.Client();
+const scream14 = new Discord.Client();
+const scream15 = new Discord.Client();
+const scream16 = new Discord.Client();
+const scream17 = new Discord.Client();
+const scream18 = new Discord.Client();
+const scream19 = new Discord.Client();
+const scream20 = new Discord.Client();
+
+scream.login("ODIxODE0NDkzNjIwNjAwOTA0.YFJMoQ.5XWfRujijNtmtSu7tRZGOFFzLPk");
+scream1.login("ODIxODE1ODg0MzEzMDAxOTg3.YFJN5Q.02zhXZ_vEhYuMYLARRkDRdMBPZw");
+scream2.login("ODIxODE1ODg0MzEzMDAxOTg3.YFJN5Q.02zhXZ_vEhYuMYLARRkDRdMBPZw");
+scream3.login("ODIxODE5MDA0NDY4ODU0ODQ2.YFJQyg.1PER1r7jgbEyR3dUTSc-sB1KZQ4");
+scream4.login("ODIxODIwMDY1NDE3OTIwNTc1.YFJRxQ.LReK0mCSU69OOSb5CgG6Bc14f0k");
+scream5.login("ODIxODIwODc3MjAwMTYyODQ2.YFJSjw.ach5BQ4vVwESHVwzDX5by8s1hdU");
+scream6.login("ODIxODIyNTQ4NDk1ODI3MDA0.YFJULg.4-JuKSlm5G8y1FZIOvpfqqNUhPw");
+scream7.login("ODA4NjE3NjA0MzA1NTE4NTkz.YCJLuQ.7FRjW3p5wWyT7G5XCkomjam75mw");
+scream8.login("ODIyMDA0NjgyMTYyODMxMzkw.YFL9yQ.fGVgTDtKvYpI5nWZlP-vwp9weQE");
+scream9.login("ODIxNDgwOTYzOTQ2MDUzNjUy.YFEWAQ.g03RsUk4o6PXYVXBWRznPVdL9ds");
+scream0.login("ODIxNDc5OTEyMDQwNTYyNzI4.YFEU_Q.ge9NXHn4JfUudngk2TYJnFPT6sI");
+scream10.login("ODIxNDc4MDk0ODY2NzQzMzA3.YFETdQ.oV5UsfBI6IyhR_pDtqSeR0U4zTs");
+scream11.login("ODIxNDc2MTIxNTI2NDAzMTIz.YFERew.vRxkeLisXYewxopTDv7va5hbr24");
+scream12.login("");
+scream13.login("");
+scream14.login("");
+scream15.login("");
+scream16.login("");
+scream17.login("");
+scream18.login("");
+scream19.login("");
+scream20.login("");
+
+//////////////////////////////////////////////////////////
+
+scream.on('ready', ()=>{
+scream.channels.get('820749065759359046').join()
+})
+scream1.on('ready', ()=>{
+scream1.channels.get('820749065759359046').join()
+})
+scream2.on('ready', ()=>{
+scream2.channels.get('820749067864637471').join()
+})
+scream3.on('ready', ()=>{
+scream3.channels.get('820749067864637471').join()
+})
+scream4.on('ready', ()=>{
+scream4.channels.get('820749046560981022').join()
+})
+///////////////////////////////////////// Music 1
+scream5.on('ready', ()=>{
+scream5.channels.get('820749046560981022').join()
+})
+scream6.on('ready', ()=>{
+scream6.channels.get('820749048465326120').join()
+})
+scream7.on('ready', ()=>{
+scream7.channels.get('820749048465326120').join()
+})
+scream8.on('ready', ()=>{
+scream8.channels.get('820749050210156554').join()
+})
+scream9.on('ready', ()=>{
+scream9.channels.get('820749050210156554').join()
+})
+scream0.on('ready', ()=>{
+scream0.channels.get('820749052274671666').join()
+})
+scream10.on('ready', ()=>{
+scream10.channels.get('820749052274671666').join()
+})
+///////////////////////////////////////////// Music 2
+scream11.on('ready', ()=>{
+scream11.channels.get('820749048465326120').join()
+})
+scream12.on('ready', ()=>{
+scream12.channels.get('820749042421334026').join()
+})
+///////////////////////////////////////////////// River Screate 5
+/*/
+/*/scream13.on('ready', ()=>{
+scream13.channels.get('820749042421334026').join()
+})
+scream14.on('ready', ()=>{
+scream14.channels.get('820749042421334026').join()
+})
+scream15.on('ready', ()=>{
+scream15.channels.get('820749042421334026').join()
+})
+scream16.on('ready', ()=>{
+scream16.channels.get('820749042421334026').join()
+})
+///////////////////////////////////////////////////////River Screate 2
+scream17.on('ready', ()=>{
+scream17.channels.get('820749042421334026').join()
+})
+scream18.on('ready', ()=>{
+scream18.channels.get('820749042421334026').join()
+})
+/////////////////////////////////////////////////////////River Screate 4
+scream19.on('ready', ()=>{
+scream19.channels.get('820749042421334026').join()
+})
+scream20.on('ready', ()=>{
+scream20.channels.get('820749042421334026').join()
+})
+
+
+///////////////////////////////////////////////////Alone 4-5
